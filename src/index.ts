0e2aec845af4c2eaf7715432b59fc074eb76389a
Piyush5802 <137916558+Piyush5802@users.noreply.github.com>
2026-02-21 17:46:05 +0530

    change

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 8000;
 
 app.use(express.json());
 
